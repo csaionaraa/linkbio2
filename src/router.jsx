63d09d3e18267249dc2c8c60327c3cae2dd8677d
Carla Saionara Araujo <109179480+csaionaraa@users.noreply.github.com>
2026-05-25 13:35:53 -0300

    teste

--- src/router.jsx
+++ src/router.jsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Entradas from "./Entradas";
-import Acessoliberado from "./acessoliberado";
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/entradas" element={<Entradas />} />
-        <Route path="/acessoliberado" element={<Acessoliberado />} />
         <Route path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
