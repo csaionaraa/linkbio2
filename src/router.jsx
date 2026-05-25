6b692689ec322f680418e89545833e436872a604
Carla Saionara Araujo <109179480+csaionaraa@users.noreply.github.com>
2026-05-25 13:45:47 -0300

    acesso

--- src/router.jsx
+++ src/router.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Entradas from "./Entradas";
+import Acesso from "./Acesso";
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/entradas" element={<Entradas />} />
+        <Route path="/acesso" element={<Acesso />} />
         <Route path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
