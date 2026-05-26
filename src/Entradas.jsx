506535b3684d08cd968c264c50bfe37f325372d0
Carla Saionara Araujo <109179480+csaionaraa@users.noreply.github.com>
2026-05-26 11:06:27 -0300

    update trackfather scrpit

--- src/Entradas.jsx
+++ src/Entradas.jsx
@@ -1,22 +1,8 @@
-import { useEffect } from 'react';
+
 import './Entradas.css'
 
 function Entradas() {
-  useEffect(() => {
-    window.TrackFatherConfig = {
-      workspace_id: '4bc8119a-4fd3-498e-b4c1-c652ba25d231',
-      lead_type: 'channel'
-    };
 
-    const script = document.createElement('script');
-    script.src = 'https://trackfather.com/trackfather.min.js?workspace_id=4bc8119a-4fd3-498e-b4c1-c652ba25d231';
-    script.async = true;
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
 
   return (
     <div className="container">
@@ -25,15 +11,28 @@ function Entradas() {
       <main className="content">
         <div className="grupo-container">
           <h1 className="grupo-titulo">Parabéns!</h1>
-          <p className="grupo-subtitulo">Você acabou de ganhar acesso gratuito!</p>
-          
-          <button className="grupo-botao" href="#" >CLIQUE AQUI PARA GARANTIR SEU ACESSO</button>
+
+          <p className="grupo-subtitulo">
+            Você acabou de ganhar acesso gratuito!
+          </p>
+
+          <button
+            class= "trackfather"
+            className="grupo-botao"
+            data-tf-track="click"
+            onClick={() => {
+              window.location.href =
+                'https://trackfather.com/trackfather.min.js?workspace_id=4bc8119a-4fd3-498e-b4c1-c652ba25d231'
+          }}
+          >CLIQUE AQUI PARA GARANTIR SEU ACESSO
+          </button>
+
           <img
             src="/buttons/termos.png"
             alt="Jogue com responsabilidade"
             className="grupo-termo"
           />
-        </div>
+        </div>       
       </main>
     </div>
   )
